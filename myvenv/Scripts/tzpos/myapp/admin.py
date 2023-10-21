692e4641640429f728b2a7d95c838ff67ace144e
AmanatAli6216 <114485020+AmanatAli6216@users.noreply.github.com>
2023-10-21 17:00:25 +0500

    search add

--- myvenv/Scripts/tzpos/myapp/admin.py
+++ myvenv/Scripts/tzpos/myapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-
-admin.site.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ['sup_name', 'phone', 'email', 'address']
+    search_fields = ('sup_name', 'phone')
+admin.site.register(Supplier, SupplierAdmin)
