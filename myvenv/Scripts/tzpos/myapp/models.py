aaac259713901685f2097bf85ec735990873637d
AmanatAli6216 <114485020+AmanatAli6216@users.noreply.github.com>
2023-10-21 16:39:09 +0500

    more commit

--- myvenv/Scripts/tzpos/myapp/models.py
+++ myvenv/Scripts/tzpos/myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Supplier(models.Model):
+    sup_name=models.CharField(max_length=200)
+    phone=models.CharField(max_length=50)
+    email=models.EmailField()
+    address=models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.sup_name
